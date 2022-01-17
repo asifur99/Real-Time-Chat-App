b82689cb809c748be0cb0d40d44362f54661fce5
Asifur Rahman <asifur2014@hotmail.com>
2022-01-16 21:38:23 -0500

    Create Chatroom when clicked on by a user

--- app/components/UserItem/UserItem.tsx
+++ app/components/UserItem/UserItem.tsx
@@ -1,27 +1,50 @@
 /**
  * This is the user screen component that is used by the application to render the users screen
  */
- import React from 'react';
- import { View, Text, Image, Pressable } from 'react-native';
- import { useNavigation } from '@react-navigation/core';
- import styles from './styles';
+import React from 'react';
+import { View, Text, Image, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import styles from './styles';
+import { ChatRoom, ChatRoomUser, User } from '../../src/models';
+import { DataStore } from '@aws-amplify/datastore';
+import { Auth } from 'aws-amplify';
  
- export default function UserItem({ user }) {
-     const navigation = useNavigation();
- 
-     const onPress = () => {
-         //Create a chatroom
-     }
- 
-     return(
-         <Pressable onPress={onPress} style={styles.container}>
-             <Image source={{uri: user.imageUri}} style={styles.image}/>
- 
-             <View style={styles.chatBox}>
-                 <View style={styles.group}>
-                     <Text style={styles.name}>{user.name}</Text>
-                 </View>
-             </View>
-         </Pressable>
-     );
- }
+export default function UserItem({ user }) {
+    const navigation = useNavigation();
+
+    const onPress = async () => {
+        // TODO: dont create a new chat room if one already exists
+
+        // Create a chatroom
+        const newChatRoom = await DataStore.save(new ChatRoom({newMessages: 0}));
+
+        // connect authenticated/current user with the room
+        const authenticatedUser = await Auth.currentAuthenticatedUser();
+        const currentUser = await DataStore.query(User, authenticatedUser.attributes.sub);
+        
+        await DataStore.save(new ChatRoomUser({
+            user: currentUser,
+            chatRoom: newChatRoom
+        }));
+
+        // connect authenticated/current user with the room 
+        await DataStore.save(new ChatRoomUser({
+            user,
+            chatRoom: newChatRoom
+        }));
+
+        navigation.navigate('ChatRoom', { id: newChatRoom.id });
+    }
+
+    return(
+        <Pressable onPress={onPress} style={styles.container}>
+            <Image source={{uri: user.imageUri}} style={styles.image}/>
+
+            <View style={styles.chatBox}>
+                <View style={styles.group}>
+                    <Text style={styles.name}>{user.name}</Text>
+                </View>
+            </View>
+        </Pressable>
+    );
+}
--- app/screens/UsersScreen.tsx
+++ app/screens/UsersScreen.tsx
@@ -1,15 +1,22 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
+import { DataStore } from '@aws-amplify/datastore';
 
 import UserItem from '../components/UserItem';
-import Users from '../dummy-data/Users';
+import { User } from '../src/models';
 
 export default function UsersScreen() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    //query users
+    DataStore.query(User).then(setUsers);
+  }, [])
 
   return (
     <View style={styles.page}>
       <FlatList 
-        data={Users} 
+        data={users} 
         renderItem={({ item }) => <UserItem user={item}/>} 
         showsVerticalScrollIndicator = {false}
       />
